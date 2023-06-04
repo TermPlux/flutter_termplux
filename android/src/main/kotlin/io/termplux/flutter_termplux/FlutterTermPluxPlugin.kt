ad153a84328b2db8fa1746387f954c3554a88f25
王泳淇 <815099246@qq.com>
2023-06-04 13:17:35 +0800

    commit

--- android/src/main/kotlin/io/termplux/flutter_termplux/FlutterTermPluxPlugin.kt
+++ android/src/main/kotlin/io/termplux/flutter_termplux/FlutterTermPluxPlugin.kt
@@ -1,8 +1,11 @@
 package io.termplux.flutter_termplux
 
 import android.content.Context
+import android.os.Build
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -14,6 +17,8 @@ import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import io.flutter.embedding.android.FlutterFragment
 import io.flutter.embedding.android.FlutterView
 import io.flutter.embedding.engine.FlutterEngine
@@ -30,11 +35,15 @@ class FlutterTermPluxPlugin : FlutterPlugin, MethodCallHandler, TermPlux, Defaul
     private lateinit var mChannel: MethodChannel
 
     private lateinit var mContext: Context
-    private lateinit var mFlutterFragment: FlutterFragment
-
+    private lateinit var mActivity: AppCompatActivity
+    private lateinit var mFragment: FlutterFragment
 
     private lateinit var mComposeView: ComposeView
+    private lateinit var mViewPager2: ViewPager2
     private lateinit var mFlutterView: FlutterView
+    private lateinit var mSplashLogo: AppCompatImageView
+    private lateinit var mRecyclerView: RecyclerView
+    private lateinit var mSettingsView: ViewPager2
 
 
     override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
@@ -44,7 +53,7 @@ class FlutterTermPluxPlugin : FlutterPlugin, MethodCallHandler, TermPlux, Defaul
 
     override fun onMethodCall(call: MethodCall, result: Result) {
         if (call.method == "getPlatformVersion") {
-            result.success("Android ${android.os.Build.VERSION.RELEASE}")
+            result.success("Android ${Build.VERSION.RELEASE}")
         } else {
             result.notImplemented()
         }
@@ -63,19 +72,16 @@ class FlutterTermPluxPlugin : FlutterPlugin, MethodCallHandler, TermPlux, Defaul
             // 移除FlutterView
             (mFlutterView.parent as ViewGroup).removeView(mFlutterView)
             // 设置全局上下文
-            mContext = requireActivity()
+            mContext = context
+            mActivity = requireActivity() as AppCompatActivity
             // 设置全局活动实例
-            mFlutterFragment = this@apply
+            mFragment = this@apply
             // 初始化ComposeView
             mComposeView = ComposeView(mContext)
             // 设置声明周期
             lifecycle.addObserver(this@FlutterTermPluxPlugin)
         }
         return mComposeView
-    }
-
-    override fun clean(flutterEngine: FlutterEngine) {
-
     }
 
     override fun run() {
@@ -85,6 +91,11 @@ class FlutterTermPluxPlugin : FlutterPlugin, MethodCallHandler, TermPlux, Defaul
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(owner: LifecycleOwner) {
         super.onCreate(owner)
+        mFlutterView.apply {
+
+        }
+
+
         mComposeView.apply {
             setContent {
                 Scaffold(
@@ -130,6 +141,14 @@ class FlutterTermPluxPlugin : FlutterPlugin, MethodCallHandler, TermPlux, Defaul
         super.onDestroy(owner)
     }
 
+    override fun clean(flutterEngine: FlutterEngine) {
+
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        mChannel.setMethodCallHandler(null)
+    }
+
     private fun findFlutterView(view: View?): FlutterView? {
         if (view is FlutterView) return view
         if (view is ViewGroup) {
@@ -140,10 +159,6 @@ class FlutterTermPluxPlugin : FlutterPlugin, MethodCallHandler, TermPlux, Defaul
             }
         }
         return null
-    }
-
-    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-        mChannel.setMethodCallHandler(null)
     }
 
     companion object {
